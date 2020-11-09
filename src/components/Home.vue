a01e46133e8db2232487a94be5d5adad506368ec
lsk <lsk@dtribe.co.kr>
2020-11-09 17:56:22 +0900

    test

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>asdfasdfasfas</h1>
+        <h2>{{data}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Home',
+    props:{
+        data: String
+    }
+}
+</script>
