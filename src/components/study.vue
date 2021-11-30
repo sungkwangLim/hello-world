12271cac2e90fd9a73c65520a38031815cba9343
sungkwangLim <limsungkwang@naver.com>
2021-11-30 13:29:40 +0900

    create 훅 사용해보기

--- src/components/study.vue
+++ src/components/study.vue
@@ -4,7 +4,15 @@
 
 <script>
 export default {
-    name:'study'
+    name:'study',
+    data(){
+        return{
+            a:1
+        }
+    },
+    created: function(){ //create 훅은 인스턴트가 생성 된 후에 호출됩니다.
+        console.log(`a is : ${this.a}`);
+    }
 }
 </script>
 
